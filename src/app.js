fe38e9e3423ff6d4de9a9c091d7877fc36414992
Diego Beker <diegobecker2007@hotmail.com>
2023-04-10 09:39:06 -0300

    add: rota GET /tweets/USERNAME

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
-    if (!username || !avatar || typeof username !== "string"|| typeof avatar !== "string") {
+    if (!username || !avatar || typeof username !== "string" || typeof avatar !== "string") {
         return res.status(400).send("Todos os campos são obrigatórios!");
     }
     usuarios.push({ username: username, avatar: avatar });
@@ -23,7 +23,7 @@ app.post("/tweets", (req, res) => {
     if (!usuario) {
         return res.status(401).send("UNAUTHORIZED");
     }
-    if (!username || !tweet || typeof username !== "string"|| typeof tweet !== "string") {
+    if (!username || !tweet || typeof username !== "string" || typeof tweet !== "string") {
         return res.status(400).send("Todos os campos são obrigatórios!");
     }
     tweets.push({ username: username, tweet: tweet });
@@ -33,18 +33,31 @@ app.post("/tweets", (req, res) => {
 app.get("/tweets", (req, res) => {
     const tweetsList = [];
     let latestTweets;
-    
+
     if (tweets.length > 10) {
         latestTweets = tweets.slice(-10);
     } else {
         latestTweets = [...tweets];
     }
-    
+
     latestTweets.forEach((tweet) => {
         const usuario = usuarios.find(usuario => usuario.username === tweet.username);
         tweetsList.push({ username: tweet.username, avatar: usuario.avatar, tweet: tweet.tweet });
     });
     res.send(tweetsList);
 });
+
+app.get("/tweets/:username", (req, res) => {
+    const {username} = req.params;
+    const tweetsList = [];
+    const userTweets = tweets.filter((t)=> t.username === username);
+    if(userTweets.length > 0){
+        const usuario = usuarios.find(usuario => usuario.username === username);
+        userTweets.forEach((tweet) => {
+            tweetsList.push({ username: tweet.username, avatar: usuario.avatar, tweet: tweet.tweet });
+        });
+    }
+    res.send(tweetsList);
+})
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT} `));
